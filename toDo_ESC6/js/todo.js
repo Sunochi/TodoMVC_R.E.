020af302a6b91ceae8cf2c920de44344f94034b6
簾内 統賀 <s01718@CG4536.local>
2018-04-18 16:25:30 +0900

    リファクタリング

--- toDo_ESC6/js/todo.js
+++ toDo_ESC6/js/todo.js
@@ -1,11 +1,17 @@
+/********************************
+@TODO 保存の動作対応 2018/04/18 簾内
+(@TODO ファイル書き出し動作対応 2018/04/18)
+*********************************/
+
 
 window.onload = function () {
-  let todo_num = 0; //@TODO localの値を代入する
-  let todo_count = 1;
+  let todo_num = 0; //現在のTodoの数
+  let todo_count = 1; //今までのTodoの数（主Key）
+
   // 常に使用しそうなDOMは最初に用意しておく
   var input_todo = document.getElementById("input_todo");
   var todo_list = document.getElementById("todo_list");
-  var under_text = document.getElementById("under_text");
+  var todo_footer = document.getElementById("todo_footer");
   var all_check_box = document.getElementById("all_check");
 
   //todoの入力のイベント
@@ -22,32 +28,30 @@ window.onload = function () {
       if(input_todo.value == ""){
         return false;
       }
-      todo_num++;
-
       //tableに追加するタグを生成
-      var tr = document.createElement("TR");
+      var tr       = document.createElement("TR");
       var td_check = document.createElement("TD");
-      var td_text = document.createElement("TD");
-      tr.id = "todo_" + todo_count;
+      var td_text  = document.createElement("TD");
+      tr.id   = "todo_" + todo_count;
       tr.name = "todo";
       tr.addEventListener("mouseover", indicateBtn);
       tr.addEventListener("mouseout", hiddenBtn);
 
       //todoのチェックボタン作成
-      var chk = document.createElement("INPUT");
+      var chk   = document.createElement("INPUT");
+      chk.value = "todo_" + todo_count;
       chk.setAttribute("type", "checkbox");
       chk.setAttribute('name', 'todo_check');
-      chk.value = "todo_" + todo_count;
 
       //todoの内容のテキストの作成
-      var todo_text = document.createElement("SPAN");
+      var todo_text       = document.createElement("SPAN");
       todo_text.innerHTML = input_todo.value;
 
       //todoを削除するボタンの作成
-      var btn = document.createElement("BUTTON");
-      btn.setAttribute("type", "button");
+      var btn   = document.createElement("BUTTON");
       btn.value = todo_count;
-      btn.name = "todo_" + todo_count;
+      btn.name  = "todo_" + todo_count;
+      btn.setAttribute("type", "button");
       btn.addEventListener('click',deleteTodo);
       btn.style.display = "none";
 
@@ -59,22 +63,27 @@ window.onload = function () {
       tr.appendChild(td_text);
       todo_list.appendChild(tr);
 
-      //todoの入力をリセット
+      //todoの入力をリセット,todoの数の管理
       input_todo.value = "";
+      todo_num++;
       todo_count++;
-      //todo_listの数が１個以上になるので、全チェックボタンを有効に
-      setAllCheckBox(true);
+      //todo_listの数が必ず１個以上になるので、全チェックボタンを有効に
+      setCheckBoxforAll(true);
   }
 
+  //Todoの行にマウスオーバーした時の削除ボタンの表示
   function indicateBtn(){
       var btn = document.getElementsByName(this.id)[0];
       btn.style.display = "";
   }
+
+  //Todoの行からマウスアウトした時の削除ボタンの非表示
   function hiddenBtn(){
       var btn = document.getElementsByName(this.id)[0];
       btn.style.display = "none";
   }
 
+  //削除ボタンを押した際の動作
   function deleteTodo(){
       var target_tr = document.getElementById("todo_" + this.value);
       target_tr.parentNode.removeChild(target_tr);
@@ -84,7 +93,13 @@ window.onload = function () {
       }
   }
 
-  function setAllCheckBox(flg){
+  //全チェックボタンの表示・非表示の切り替え
+  function setCheckBoxforAll(flg){
       all_check_box.disabled = !flg;
+  }
+
+  //テーブルのフッターの整形/表示・非表示
+  function setTodoFooter(flg){
+
   }
 };
