309c1ef243baf9220ef7134cda880663a8743086
簾内統賀 <sunouchi@sunouchi.local>
2018-05-13 23:51:10 +0900

    Backbone.jsの更新。backboneの例にあるサイトを参考に作成途中

--- todoMVC/js/todo_backbone.js
+++ todoMVC/js/todo_backbone.js
@@ -1,6 +1,6 @@
 /*参考: http://backbonejs.org/docs/todos.html*/
 
-
+// Model
 const Task = Backbone.Model.extend({
     defaults: function(){
         return {
@@ -15,93 +15,101 @@ const Task = Backbone.Model.extend({
     }
 });
 
+// Collection
 const TaskList = Backbone.Collection.extend({
     model: Task,
+    localStorage: new Backbone.LocalStorage("todos"),
     comparator: "number",
 
     done: function() {
-      return this.where({done: true});
+      return this.filter(function(todo){return todo.get('done')});
     },
     remaining: function() {
       return this.where({done: false});
     },
 });
 
-// the Root View (controller view)
-const ParentView = Backbone.View.extend({
-  initialize: () => {
-    // create models and collections
-    this.taskCollection = new TaskCollection();
+var Todos = TaskList;
 
-    // create sub views
-    this.tasksView = new TasksView({ collection: this.taskCollection })
-    this.addTaskView = new AddTaskView()
-
-    // set event handlers
-    this.addTaskView.on(EVENT_SUBMITTED, (value) => {
-      const model = new TaskModel({
-        title: value
-      })
-      this.taskCollection.add(model)
-    })
-  },
-})
-
-
-// this has input form (presentational view)
-const AddTaskView = Backbone.View.extend({
-  el: '',
-
-  events: {
-    submit: 'onSubmitted'
-  },
-
-  onSubmitted: (e) => {
-    this.trigger(EVENT_SUBMITTED, $('#input_todo').val())
-  },
-})
-
-
-// mixed presentational and controller
-const TasksView = Backbone.View.extend({
-  el: '',
-
-  // NOTE: this class does NOT have any "taskViewList" or something,
-  // because this class has collection, collection includes models,
-  // and the each model is attached to the view, so we can access all views
-  initialize: () => {
-    this.collection.on('add', (model) => {
-      const newView = new TaskView(model)
-
-      // set event handlers
-      newView.once(EVENT_TASK_DELETED, () => {
-        newView.remove()
-        this.collection.remove(model)
-      })
-
-      // render
-      this.$el.append(this.newView.$el);
-      newView.render()
-    })
-  },
-})
-
-
-// single task view (presentational view)
+// Todo１つ（行）
 const TaskView = Backbone.View.extend({
-  el: '#todo_list',
-
+  tagName: 'tr',
+  template: _.template($('#task-template').html()),
   events: {
-    'click .delete': 'onDeleteClicked'
+    'click .todo_delete' : 'onDeleteClicked',
+    'click .todo_chbox'  : 'changeDone',
+    'dblclick .todo_text': 'edit',
+    'keypress .todo_edit': 'onEnterPress',
+    'blur .todo_edit'    : 'closeEdit'
   },
-
-  onDeleteClicked: (e) => {
-    this.trigger(EVENT_TASK_DELETED)
+  initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
   },
 
   render: function(){
-      $(this.el).html(_.template($('#task-template').html(), {name: 'foo', text: 'bar'}));
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass('done', this.model.get('done'));
+      this.input = this.$('.edit');
+      return this;
   },
+
+  onDeleteClicked: function() {
+    this.model.destroy();
+  },
+
+  changeDone: function(){
+      this.model.toggle();
+  },
+
+  edit: function(){
+      this.$el.addClass("edit");
+      this.input.focus();
+  },
+
+  onEnterPress: function(){
+      if(e.keyCode == 13) this.closeEdit();
+  },
+
+  closeEdit: function(){
+      var value = this.input.val();
+      if(value){
+          this.model.save({title: value});
+          this.$el.removeClass("edit");
+      }
+  }
+});
+
+// the Root View (controller view)
+const ParentView = Backbone.View.extend({
+  el: $("#todo_table"),
+  infoTemplate: _.template($("#info-template").html()),
+
+  events: {
+      "keypress #input_todo"      : "createOnEnter",
+      "click #completed_clear_btn": "clearCompleted",
+      "click #all_check"          : "toggleAllCheck",
+      "click #change_display_all_btn"      : "dispAll",
+      "click #change_display_active_btn"   : "dispActive",
+      "click #change_display_completed_btn": "dispComp",
+  },
+  initialize: function(){
+    this.input       = this.$("new-todo");
+    this.allCheckBox = this.$("#all_check");
+
+    this.listenTo(Todos, 'add', this.addOne);
+    this.listenTo(Todos, 'all', this.render);
+
+    this.footer = this.$('footer');
+    this.main   = $('#todo_list');
+  },
+  addOne: function(){
+
+  },
+  render: function(){
+
+  }
+
 })
 
 var pview = new ParentView;
